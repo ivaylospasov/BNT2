f1d02da2ca7500e85bc257c05981aac12fb75718
Ivaylo Spasov <ivaylo.pif@gmail.com>
2016-12-11 21:00:41 +0200

    Create join_files.sh

---
+++ join_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+current_dir=`pwd`
+
+now=$(date +"_%Y%m%d")
+
+output_file=$1$now.flv
+
+all_files=$current_dir/*.flv
+
+my_list=$current_dir/mylist.txt
+
+for f in $current_dir/*.flv; do echo "file '$f'" >> $my_list; done
+
+ffmpeg -f concat -safe 0 -i $my_list -c copy $output_file
